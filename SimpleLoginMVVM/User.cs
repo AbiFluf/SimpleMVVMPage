5b850037c5e4837491c124fede36053e7e71e2f1
Abigail <Abigiya24@gmail.com>
2019-05-29 09:57:00 +0300

    Simple MVVM Page

---
+++ SimpleLoginMVVM/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SimpleLoginMVVM
+{
+    sealed class User
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+
+
+}
